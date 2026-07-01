2fb50f787555682bca2eb75da6179bfb1d074169
Ivan Nafrizaldi <142771799+i-nafrizaldi@users.noreply.github.com>
2026-07-01 19:23:44 +0800

    add nextjs (#2)

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      HOME PAGE
+      <Button className="bg-red-400 cursor-pointer hover:bg-amber-100">
+        ini buttonnua
+      </Button>
+    </div>
+  );
+}
